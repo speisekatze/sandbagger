36c86f315fd9c9e797949a8aca7f47be7398e0e6
Thomas Walter <speisekatze@gmail.com>
2020-03-08 04:12:17 +0100

    fixed keyboard handling and changes to reflect refectoring

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ Created on Sat Mar 28 23:37:56 2015
 @author: n4p
 """
 
-from lib import *
-from lib.httpServer import *
 import time
-import sys
+from lib.httpServer import http_request
+from lib.httpServer import httpServer
+#from lib import request_handler
+from lib import config
 
-conf = Config.Config('Config/sandbagger.conf')
 
-handler = httpRequest.httpRequest
-handler.externHandler = RequestHandler.RequestHandler(conf)
-httpd = httpServer.httpServer(handler,int(conf.value('Server/port')))
 
-httpd.start()
-time.sleep(600)
-httpd.stop()
 
+Handler = http_request.HttpRequest
+#Handler.externHandler = request_handler.RequestHandler(CONF)
+HTTPD = httpServer.httpServer(Handler, int(config.Config().value('Server/port')))
+
+HTTPD.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt as error:
+    print('Server shutting down')
+HTTPD.stop()
