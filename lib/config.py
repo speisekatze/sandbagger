b091217b76f73f159173adbc9c10aed323278583
Thomas Walter <speisekatze@gmail.com>
2020-03-08 04:05:04 +0100

    pylint and shifting things around

---
+++ lib/config.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Config class reading xml config file
+Created on Sun Mar 29 22:50:29 2015
+
+@author: n4p
+"""
+
+import xml.etree.ElementTree as ET
+
+class Config():
+    """ wrapper for reading config xml with xpath """
+    root = None
+    tree = None
+
+    def __init__(self, filename = 'Config/sandbagger.conf'):
+        self.tree = ET.parse(filename)
+        self.root = self.tree.getroot()
+
+    def value(self, xpath):
+        """ return first found option matching xpath """
+        return self.root.findall(xpath)[0].text
+
+    def valuelist(self, xpath):
+        """ return list of config options """
+        return self.root.findall(xpath)
